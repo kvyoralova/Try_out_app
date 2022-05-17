15805695d0794475ab66f49c3b3efb92787f72e6
Vstrijova <101127668+Vstrijova@users.noreply.github.com>
2022-05-17 16:43:29 +0200

    Update Experiments.py

--- Experiments.py
+++ Experiments.py
@@ -11,6 +11,4 @@ if language == 'Русский':
   placechoice = st.selectbox("Куда ты хочешь пойти сегодня? Bыбери одно из указанных мест:", ('Площадка для игр', 'Школа', 'Магазин'))
   if placechoice == 'Площадка для игр':
     st.header("Русский")
-    st.write(pd.DataFrame({
-     'first column': ["1. Пойдем в парк", "2. Давай играть в прятки", "3. Давай покатаемся на качелях"],
-     'second column': ["Andiamo al parco", "Giochiamo a nascondino", "Andiamo sull'altalena"],}))
+    st.write(pd.DataFrame({'first column': ["1. Пойдем в парк", "2. Давай играть в прятки", "3. Давай покатаемся на качелях"],'second column': ["Andiamo al parco", "Giochiamo a nascondino", "Andiamo sull'altalena"]})
