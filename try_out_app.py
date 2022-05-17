2f32c7e980a728ff044439a904ea862cc9e72ddf
Vstrijova <101127668+Vstrijova@users.noreply.github.com>
2022-05-17 23:19:33 +0200

    Update try_out_app.py

--- try_out_app.py
+++ try_out_app.py
@@ -1,8 +1,8 @@
 import streamlit as st
 from PIL import Image
 from gtts import gTTS
-#from google.transliteration import transliterate_text
-from transliterate import translit, get_available_language_codes
+from google.transliteration import transliterate_text
+#from transliterate import translit, get_available_language_codes
 
 
 st.title("Итальянский разговорник для детей - Італійський розмовник для дітей")
@@ -15,6 +15,8 @@ if language == 'Русский':
     image1 = Image.open('playground.jpg')
     st.image(image1, caption='Photo by Pond Juprasong on Unsplash')
     sentence1 = "1. Пойдем в парк" 
+    sentence2 = "2. Давай играть в прятки"
+    sentence3 = "3. Давай покатаемся на качелях"
     col1, col2 = st.columns(2)
     with col1:
       st.header("Русский")
@@ -28,10 +30,12 @@ if language == 'Русский':
       tts1.save('your_file.mp3')
       audio_file = open('your_file.mp3', 'rb')
       st.audio(data=audio_file, format="audio/mp3", start_time = 0)
-      st.write(translit(textpl, 'ru')
+      #st.write(translit(textpl1, 'ru')
+      result1 = transliterate_text(textpl1,  "ru")
+      st.write(result1)
       
     with col1:
-      st.write("2. Давай играть в прятки")
+      st.write(sentence2)
     with col2:
       textpl2 = "Giochiamo a nascondino"
       st.write("2. ", textpl2)
@@ -39,10 +43,12 @@ if language == 'Русский':
       tts1.save('your_file.mp3')
       audio_file = open('your_file.mp3', 'rb')
       st.audio(data=audio_file, format="audio/mp3", start_time = 0)
-      st.write(translit(textpl2 'ru')
+      result2 = transliterate_text(textpl2,  "ru")
+      st.write(result2)
+      #st.write(translit(textpl2 'ru')
       
     with col1:  
-      st.write("3. Давай покатаемся на качелях")
+      st.write(sentence3)
     with col2:           
       textpl3 = "Andiamo sull'altalena"
       st.write("3.", textpl3)
@@ -50,8 +56,8 @@ if language == 'Русский':
       tts1.save('your_file.mp3')
       audio_file = open('your_file.mp3', 'rb')
       st.audio(data=audio_file, format="audio/mp3", start_time = 0)
-      #result3 = transliterate_text(textpl3,  "ru")
-      #st.write(result3)
+      result3 = transliterate_text(textpl3,  "ru")
+      st.write(result3)
   
 if language == 'Yкраїнський':
   placecoicheuk = st.selectbox("Куди ти хочеш піти сьогодні? Bыбери одно з вказаних місць:", ('Майданчик для ігор', 'Школа', 'Магазин'))
