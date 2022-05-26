3769e9502bac5110871a5dab6972008cfc6aa533
Vstrijova <101127668+Vstrijova@users.noreply.github.com>
2022-05-26 14:01:54 +0200

    Update try_app_Karin.py

--- try_app_Karin.py
+++ try_app_Karin.py
@@ -10,26 +10,27 @@ st.title("Італійський розмовник для дітей - Итал
 language = st.radio( "Виберіть мову - Выберите язык" , ('Русский', 'Yкраїнський'))
 
 def trans(sentence):
-  col2, col3, col4 = st.columns(3)
-  with col2:
-    translation = translator.translate(sentence, dest='it')
-    translation = translation.text
-    st.write(translation)
-  with col3:
-    tts1=gTTS(translation, lang = 'it')
-    tts1.save('your_file.mp3')
-    audio_file = open('your_file.mp3', 'rb')
-    st.audio(data=audio_file, format="audio/mp3", start_time = 0)
+  translation = translator.translate(sentence, dest='it')
+  translation = translation.text
+  st.write(translation)
+ 
+def audio(text)
+  tts1=gTTS(translation, lang = 'it')
+  tts1.save('your_file.mp3')
+  audio_file = open('your_file.mp3', 'rb')
+  st.audio(data=audio_file, format="audio/mp3", start_time = 0)
+  
+  
+def tranllit(translation):
   if language == 'Русский':
     lan = 'ru'
   elif language == 'Yкраїнський':
     lan = 'uk'
   else:
     pass
-  with col4:
-    result = translit(translation, lan)
-    st.write(result)
-    
+  translit(translation, lan)
+  st.write(result)
+  
 
 if language == 'Русский':
   placechoice = st.selectbox("Куда ты хочешь пойти сегодня? Bыбери одно из указанных мест:", ('Площадка для игр', 'Школа', 'Магазин'))
@@ -41,7 +42,7 @@ if language == 'Русский':
     sentence3 = "Давай покатаемся на качелях"
     sentence4 = "Пойдем на горку"
     
-    col1, col2 = st.columns(2)
+    col1, col2, col3, col4 = st.columns(4)
     with col1:
       st.header("Русский")
       st.write(sentence1)
@@ -50,59 +51,56 @@ if language == 'Русский':
       st.header("Итальянский")
       trans(sentence1)
       
-    with col1:
-      st.write(sentence2)
-    with col2:
-      trans(sentence2)
+    with col3:
+      aduio(sentence1)
       
-    with col1:  
-      st.write(sentence3)
-    with col2:           
-      trans(sentence3)
+    with col4:  
+      tranllit(sentence3)
+ 
       
-    with col1:  
-      st.write(sentence4)
-    with col2:           
-      trans(sentence4)
+   # with col1:  
+    #  st.write(sentence4)
+    #with col2:           
+     # trans(sentence4)
       
-  if placechoice == 'Школа':
-    image2 = Image.open('school.jpg')
-    st.image(image2, caption='Photo by Kenny Eliason on Unsplash')
+  #if placechoice == 'Школа':
+   # image2 = Image.open('school.jpg')
+    #st.image(image2, caption='Photo by Kenny Eliason on Unsplash')
     #sentence6 = '
     
   
-if language == 'Yкраїнський':
-  placechoiceuk = st.selectbox("Куди ти хочеш піти сьогодні? Bыбери одно з вказаних місць:", ('Майданчик для ігор', 'Школа', 'Магазин'))
-  if placechoiceuk == 'Майданчик для ігор':
-    image1 = Image.open('playground.jpg')
-    st.image(image1, caption='Photo by Pond Juprasong on Unsplash')
-    sentence1uk = "1. Підемо в парк"
-    sentence2uk = "2. Давай пограємо в хованки"
-    sentence3uk = "3. Xодімо на гойдалки"
-    sentence4uk = "4. Підемо на гірку"
+#if language == 'Yкраїнський':
+ # placechoiceuk = st.selectbox("Куди ти хочеш піти сьогодні? Bыбери одно з вказаних місць:", ('Майданчик для ігор', 'Школа', 'Магазин'))
+  #if placechoiceuk == 'Майданчик для ігор':
+   # image1 = Image.open('playground.jpg')
+    #st.image(image1, caption='Photo by Pond Juprasong on Unsplash')
+    #sentence1uk = "1. Підемо в парк"
+    #sentence2uk = "2. Давай пограємо в хованки"
+    #sentence3uk = "3. Xодімо на гойдалки"
+    #sentence4uk = "4. Підемо на гірку"
     
-    col3, col4 = st.columns(2)
-    with col3:
-      st.header("Yкраїнський")
-      st.write(sentence1uk)
-    with col4:
-      st.header("Італійський")
-      trans(sentence1uk)
+    #col3, col4 = st.columns(2)
+    #with col3:
+     # st.header("Yкраїнський")
+      #st.write(sentence1uk)
+    #with col4:
+     # st.header("Італійський")
+      #trans(sentence1uk)
       
-    with col3:
-      st.write(sentence2uk)
-    with col4:
-      trans(sentence2uk)
+    #with col3:
+     # st.write(sentence2uk)
+    #with col4:
+     # trans(sentence2uk)
       
-    with col3:
-      st.write(sentence3uk)
-    with col4:           
-      trans(sentence3uk)
+    #with col3:
+     # st.write(sentence3uk)
+    #with col4:           
+     # trans(sentence3uk)
      
-    with col3:
-      st.write(sentence4uk)
-    with col4:           
-      trans(sentence4uk)
-  if placechoiceuk == 'Школа':
-    image2 = Image.open('school.jpg')
-    st.image(image2, caption='Photo by Kenny Eliason on Unsplash') 
+    #with col3:
+     # st.write(sentence4uk)
+    #with col4:           
+     # trans(sentence4uk)
+  #if placechoiceuk == 'Школа':
+   # image2 = Image.open('school.jpg')
+    #st.image(image2, caption='Photo by Kenny Eliason on Unsplash') 
