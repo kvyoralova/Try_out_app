cac868b1d7a48aaae52c82acec21e155df8a5abb
Vstrijova <101127668+Vstrijova@users.noreply.github.com>
2022-06-12 15:02:42 +0200

    Update for_loop.py

--- for_loop.py
+++ for_loop.py
@@ -54,34 +54,35 @@ phrases_ru = {'Площадка для игр': [{'Пойдем в парк' : '
 
 for key in phrases_ru.keys():
   for el in placechoice:
-    for phrasecouple in phrases_ru.values():
-      for el in phrasecouple:
-        for (key, value) in el.items():
-          col1, col2, col3, col4 = st.columns(4)
-          with col1:
-            st.write(key)
-          with col2:
-            translation = translator.translate(key, dest='it')
-            translated_text= translation.text
-            if translated_text != value:
-              translated_text = value
-            else:
-              translated_text = translated_text
-            st.write(translated_text)
-          with col3:
-            tts1=gTTS(translated_text, lang = 'it')
-            tts1.save('your_file.mp3')
-            audio_file = open('your_file.mp3', 'rb')
-            st.audio(data=audio_file, format="audio/mp3", start_time = 0)
-          with col4:
+    if el == key: 
+      for phrasecouple in phrases_ru.values():
+        for el in phrasecouple:
+          for (key, value) in el.items():
+            col1, col2, col3, col4 = st.columns(4)
+            with col1:
+              st.write(key)
+            with col2:
+              translation = translator.translate(key, dest='it')
+              translated_text= translation.text
+              if translated_text != value:
+                translated_text = value
+              else:
+                translated_text = translated_text
+              st.write(translated_text)
+            with col3:
+              tts1=gTTS(translated_text, lang = 'it')
+              tts1.save('your_file.mp3')
+              audio_file = open('your_file.mp3', 'rb')
+              st.audio(data=audio_file, format="audio/mp3", start_time = 0)
+            with col4:
       #if language == 'Русский':
         #lan = 'ru'
       #elif language == 'Yкраїнський':
         #lan = 'uk'
       #else:
         #pass
-            transliterated_text = translit(translated_text, 'ru')
-            st.write(transliterated_text)
+              transliterated_text = translit(translated_text, 'ru')
+              st.write(transliterated_text)
 else:
   pass
 
